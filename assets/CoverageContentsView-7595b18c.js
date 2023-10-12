65d7052fcaa63af10e33697ca477a10685536240
jubeychen <107476355+jubeyChen@users.noreply.github.com>
2023-10-12 17:10:20 +0800

    Add files via upload

---
+++ assets/CoverageContentsView-7595b18c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-d08dda68.js";const r={};function t(n,c){return" 保障內容 "}const _=e(r,[["render",t]]);export{_ as default};
---
+++ assets/CoverageInquiryView-ddd36d3a.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-d08dda68.js";const r={};function c(n,t){return" 保障查詢 "}const o=e(r,[["render",c]]);export{o as default};
