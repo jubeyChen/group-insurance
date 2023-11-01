d61346ff58768205f27e237ea74c48dd6b02bbd7
jubeychen <107476355+jubeyChen@users.noreply.github.com>
2023-11-01 16:12:05 +0800

    Add files via upload

---
+++ assets/AccountManagementView-c14304bb.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-e2121f3e.js";const c={};function n(t,r){return" 我是帳號管理 "}const o=e(c,[["render",n]]);export{o as default};
---
+++ assets/ReportQueryView-37c2871f.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-e2121f3e.js";const r={};function t(c,_){return" 報表查詢 "}const o=e(r,[["render",t]]);export{o as default};
