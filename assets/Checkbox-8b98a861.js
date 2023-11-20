0eb86b6328f5368934878967306486c50d6e494c
jubeychen <107476355+jubeyChen@users.noreply.github.com>
2023-11-20 10:32:05 +0800

    Add files via upload

---
+++ assets/Checkbox-8b98a861.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Checkbox.vue_vue_type_style_index_0_scoped_5953a614_lang-848763a0.js";import{_}from"./index-597ec5e4.js";const r=_(o,[["__scopeId","data-v-5953a614"]]);export{r as N};
---
+++ assets/icon_consent-da7b8deb.js
@@ -0,0 +1,1 @@
+const n="/group-insurance/images/icons/icon_consent.png";export{n as _};
