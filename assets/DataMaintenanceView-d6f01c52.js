5431e77953dcbc662934c303bcf49946af1024a1
jubeychen <107476355+jubeyChen@users.noreply.github.com>
2023-12-08 16:31:36 +0800

    Add files via upload

---
+++ assets/DataMaintenanceView-d6f01c52.js
@@ -0,0 +1,1 @@
+import{d as t,a8 as a,o,c as n,h as r,b as c,F as s}from"./index-72eb97a3.js";const l=t({__name:"DataMaintenanceView",setup(_){return(m,p)=>{const e=a("router-view");return o(),n(s,null,[r(" 我是資料維護 "),c(e)],64)}}});export{l as default};
